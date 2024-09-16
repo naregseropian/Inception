52792e3090cce89758af8c039661a439bc8c96cd
Nareg Seropian <naregseropian@outlook.com>
2024-09-16 16:09:11 +0300

    it works :-)

--- srcs/requirements/mariadb/tools/auto_config.sh
+++ srcs/requirements/mariadb/tools/auto_config.sh
@@ -1,33 +1,63 @@
 #!/bin/sh
 
-set -e
+if [ ! -f "/tmp/init.sql" ]; then
 
-# Start MySQL server in the background
-mysqld_safe --datadir=/var/lib/mysql &
+    # Check if the database exists
+    if ! mariadb --user=root --password="$SQL_ROOT_PASSWORD" -e "USE $SQL_DATABASE;" 2>/dev/null; then
+        # If the database does not exist, create the init.sql file with instructions to set up the database
+        cat << EOF > /tmp/init.sql
+CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;
+CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+    else
+        mariadb shutdown
+        echo "FLUSH PRIVILEGES;" > /tmp/init.sql
+    fi
+fi
 
-# Wait for the server to start
-until mysqladmin ping --silent; do
-    echo "Waiting for MariaDB to start..."
-    sleep 2
-done
+# Execute the passed commands
+exec "$@"
 
-# create a database (if the database does not exist)
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-# create an user with a password (if the user does not exist)
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# if [ -z "$(ls -A /var/lib/mysql)" ]; then
+#     mysql_install_db --datadir="/var/lib/mysql"
+# else
+#     echo "Database already exists, skipping mysql_install_db."
+# fi
 
-# give all privileges to the user
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mariadbd & MARIADB_PID=$!
 
-#modify sql database
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# for i in {30..0}; do
+# 	if mariadb -e "SELECT '1';" &> /dev/null; then
+# 		echo 'mariadb init started'
+# 		break
+# 	fi
+# 	echo 'mariadb init starting...'
+# 	sleep 1
+# done
 
-#reload the database
-mysql -e "FLUSH PRIVILEGES;"
+# if [ "$i" = 0 ]; then
+# 	echo >&2 'mariadb init failed'
+# 	exit 1
+# fi
 
-#shutdown
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+# # More secure installation, and to avoid anonymous user interfering with WP user on healthcheck
+# mariadb -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${SQL_ROOT_PASSWORD}');"
+# mariadb -e "DELETE FROM mysql.user WHERE User='';"
+# mariadb -e "DROP DATABASE IF EXISTS test;"
+# mariadb -e "DELETE FROM mysql.db where Db='test' OR Db='test\\_%'"
+# mariadb -e "FLUSH PRIVILEGES;"
 
-# Start the MariaDB server in the foreground
-exec mysqld_safe --datadir=/var/lib/mysql
+# # Wp database and user init
+# mariadb -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+# mariadb -e "CREATE USER IF NOT EXISTS '${ADMIN_USER}'@'%' IDENTIFIED BY '${ADMIN_PASSWORD}';"
+# mariadb -e "GRANT ALL ON ${SQL_DATABASE}.* TO '${ADMIN_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+# kill -s TERM "$MARIADB_PID"
+
+# wait "$MARIADB_PID"
+
+# exec mariadbd-safe
--- srcs/requirements/wordpress/tools/wordpress-entrypoint.sh
+++ srcs/requirements/wordpress/tools/wordpress-entrypoint.sh
@@ -1,19 +1,25 @@
-#!bin/bash
-cd /var/www/html
+#!/bin/sh
 
-# healthycheck
-until mysqladmin ping --host=mariadb:3306 --silent; do
-    echo "Waiting for MariaDB to start..."
-    sleep 2
-done
-
-if [ ! -f wp-config.php ]; then
-    wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_ROOT_PASSWORD \
-    					--dbhost=mariadb:/var/run/mysqld/mysqld.sock --allow-root
-
-sleep 2
-wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --allow-root >> /log.txt
+if [ ! -f wp-config-sample.php ]; then
+	wp core download --skip-content
 fi
 
-exec /usr/sbin/php-fpm82 -F
+if [ ! -f wp-config.php ]; then
+    wp config create \
+    --dbname=${SQL_DATABASE} \
+    --dbuser=${SQL_USER} \
+    --dbhost="mariadb:3306"
+    # --dbpass=${SQL_PASSWORD} \
+    # --prompt=dbpass < /run/secrets/wp_db_password
+
+	wp core install --url=${DOMAIN_NAME} --title="${SITE_TITLE}" \
+        --url=nseropia.42.fr \
+		--admin_user="${ADMIN_USER}" \
+        --admin_password="${ADMIN_PASSWORD}" \
+		--admin_email="${ADMIN_EMAIL}" --skip-email
+	wp user create "${USER1_LOGIN}" "${USER1_MAIL}" --user_pass="${USER1_PASS}" --role="editor"		# --prompt=user_pass < /run/secrets/wp_user_password
+
+	wp theme install ${WP_THEME:=twentytwentythree} --activate --allow-root
+fi
+
+exec "$@"
